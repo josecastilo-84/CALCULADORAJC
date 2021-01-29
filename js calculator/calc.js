b157c1778eb8c1b00d1b6272e9205532088d5275
josecastilo-84 <77947918+josecastilo-84@users.noreply.github.com>
2021-01-28 22:44:26 -0500

    Add files via upload
    Realizacion de avance de codigo de calculadora

---
+++ js calculator/calc.js
@@ -0,0 +1,68 @@
+
+//alert("Bienvenidos");
+//swal("Bienvenidos");
+
+//console.log("PRUEBA HOLA MUNDO");
+
+function updateInput(newValue, clear=false){
+    var div=document.getElementById("data");
+
+    div.value=clear ? newValue:`${div.value}${newValue}`;
+
+ /*if(clear){
+    div.value=newValue;
+ }else{
+    div.value=`${div.value}${newValue}`;
+
+    //div.value+newValue
+    */
+}
+
+function printOperator(data){
+    updateInput(`${data.value}`);
+    //' '+data.value+' '
+}
+
+function printNumber(data){
+    updateInput(data.value);
+}
+
+function clearInput(){
+    updateInput(array,true);
+}
+
+function printOperation(){
+   var data=document.getElementById("data").value;
+   var array=validateOperator(data.split(""));
+   updateInput(' ',true);
+}
+ 
+function validateOperator(arrayData){
+   var resultado;
+   var numero1,numero2;
+    
+   numero1=Number(arrayData[0]);
+   numero2=Number(arrayData[2]);
+
+   switch (arrayData[1]){
+       case"+":
+           resultado=numero1+numero2;
+           break;
+        case"-":
+           resultado=numero1+numero2;
+           break;   
+           
+        case"*":
+           resultado=numero1+numero2;
+           break;  
+
+        case"/":
+           resultado=numero1+numero2;
+           break;
+
+    }
+
+    return resultado;
+
+}
+
